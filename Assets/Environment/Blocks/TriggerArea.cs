15e57f2c8493c5ba33bef3776d4b20bdd0884b43
ddmills <mills.dalton@gmail.com>
2019-09-17 17:11:52 -0500

    Add moving platform

---
+++ Assets/Environment/Blocks/TriggerArea.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//This script keeps track of all rigidbodies that enter and leave the trigger collider attached to this gameobject;
+//It is used by 'MovingPlatform' to detect and move rigidbodies standing on top of it;
+public class TriggerArea : MonoBehaviour {
+
+	public List <Rigidbody> rigidbodiesInTriggerArea = new List<Rigidbody>();
+
+	//Check if the collider that just entered the trigger has a rigidbody attached and add it to the list;
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.attachedRigidbody != null)
+		{
+			rigidbodiesInTriggerArea.Add(col.attachedRigidbody);
+		}
+	}
+
+	//Check if the collider that just left the trigger has a rigidbody attached and remove it from the list;
+	void OnTriggerExit(Collider col)
+	{
+		if(col.attachedRigidbody != null)
+		{
+			rigidbodiesInTriggerArea.Remove(col.attachedRigidbody);
+		}
+	}
+
+}
